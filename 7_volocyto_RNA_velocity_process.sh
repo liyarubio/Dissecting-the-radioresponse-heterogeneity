4d618a29f16be793b422a0e7ca9f5537ce95e310
liyarubio <109122784+liyarubio@users.noreply.github.com>
2024-10-25 16:56:25 +0800

    Delete 7_volocyto_RNA_velocity_process.sh

--- 7_volocyto_RNA_velocity_process.sh
+++
@@ -1,5 +0,0 @@
-velocyto run10x -m /media/s312/LCH/TH/Velocity/data/GRCh38_rmsk_1.gtf/GRCh38_rmsk_1.gtf /media/s312/LCH/TH/IR_2h /media/s312/LCH/TH/Velocity/data/refdata-gex-GRCh38-2020-A/refdata-gex-GRCh38-2020-A/genes/genes.gtf
-
-velocyto run10x -m /media/s312/LCH/TH/Velocity/data/GRCh38_rmsk_1.gtf/GRCh38_rmsk_1.gtf /media/s312/LCH/TH/IR_6h /media/s312/LCH/TH/Velocity/data/refdata-gex-GRCh38-2020-A/refdata-gex-GRCh38-2020-A/genes/genes.gtf
-
-velocyto run10x -m /media/s312/LCH/TH/Velocity/data/GRCh38_rmsk_1.gtf/GRCh38_rmsk_1.gtf /media/s312/LCH/TH/NC_mix /media/s312/LCH/TH/Velocity/data/refdata-gex-GRCh38-2020-A/refdata-gex-GRCh38-2020-A/genes/genes.gtf
